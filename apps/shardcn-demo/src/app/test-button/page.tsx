ac40728d63df213159fb73796156db2abb391236
LiuJie <1547698569@qq.com>
2025-11-19 00:37:53 +0800

    feat: support mdx

---
+++ apps/shardcn-demo/src/app/test-button/page.tsx
@@ -0,0 +1,9 @@
+import TestButton from '@/components/local/test-button/test-button.mdx'
+
+export default function Page() {
+  return (
+    <div>
+      <TestButton />
+    </div>
+  )
+}
--- apps/shardcn-demo/src/components/local/test-button.tsx
+++
@@ -1,5 +0,0 @@
-import {Button} from "@/components/ui/button";
-
-function TestButton(){
-    return <Button>Hello shardcn ！</Button>
-}
